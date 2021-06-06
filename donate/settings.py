ef65f67bdeb853f7aea2e76ab8a6b5ca65c69d68
Sakshi-jhaa <69498793+Sakshi-jhaa@users.noreply.github.com>
2021-06-06 12:03:35 +0530

    Update settings.py

--- donate/settings.py
+++ donate/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'donate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dcc',
         'USER':'postgres',
         'PASSWORD':'$akshijh@1234',
